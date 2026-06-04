2bef1224802e2d3c467cb9784848f04385c1eb18
zzzxxxxxxxxxx <2409974882@qq.com>
2026-06-04 16:35:28 +0800

    personal: switch build variant from userdebug to user

--- buildbot_treble_personal.sh
+++ buildbot_treble_personal.sh
@@ -159,7 +159,7 @@ export WITH_SU=false
 mkdir -p ~/build-output/
 
 buildVariant() {
-	lunch ${1}-userdebug
+	lunch ${1}-user
 	make installclean
 	make -j$(nproc --all) systemimage
 	make vndk-test-sepolicy
